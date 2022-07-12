9a033b18eeb7e18463300366b23b1722cf0cc28d
Gomez Enzo <gomezenzo1128@gmail.com>
2022-07-12 17:58:32 -0300

    perform the sliver appbar the details screen

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -7,14 +7,42 @@ class DetailsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final String movie =
         ModalRoute.of(context)?.settings.arguments.toString() ?? 'no-movie';
-    return const Scaffold(
+    return Scaffold(
         body: CustomScrollView(
       slivers: [
-        SliverAppBar(
-          backgroundColor: Colors.indigo,
-          expandedHeight: 200,
-        ),
+        _CustomAppBar(),
       ],
     ));
+  }
+}
+
+class _CustomAppBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      backgroundColor: Colors.indigo,
+      expandedHeight: 200,
+      floating: false,
+      pinned: true,
+      flexibleSpace: FlexibleSpaceBar(
+        centerTitle: true,
+        titlePadding: const EdgeInsets.all(0),
+        title: Container(
+            width: double.infinity,
+            color: Colors.red,
+            alignment: Alignment.bottomCenter,
+            child: const Text(
+              'movie.title',
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            )),
+        background: const FadeInImage(
+          placeholder: AssetImage('assets/loading.gif'),
+          image: NetworkImage('https://via.placeholder.com/500x300'),
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
   }
 }
