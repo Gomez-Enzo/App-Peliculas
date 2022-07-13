b4be0de33e5febb5204dab1ceef9d7d1acdc64e0
Gomez Enzo <gomezenzo1128@gmail.com>
2022-07-13 15:05:20 -0300

    start the provider

--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -1,5 +1,11 @@
-class MoviesProvider {
+import 'package:flutter/widgets.dart';
+
+class MoviesProvider extends ChangeNotifier {
   MoviesProvider() {
     print('Movies provider inicializado');
+    this.getOnDisplayMovies();
+  }
+  getOnDisplayMovies() async {
+    print('getOnDisplayMovies');
   }
 }
