13b59c3f1e373d96509fdc7566afbf98ded84c7e
Gomez Enzo <gomezenzo1128@gmail.com>
2022-07-13 15:28:27 -0300

    start the http

--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -1,11 +1,25 @@
+import 'dart:convert';
+
 import 'package:flutter/widgets.dart';
+import 'package:http/http.dart' as http;
 
 class MoviesProvider extends ChangeNotifier {
+  String _apiKey = '1865f43a0549ca50d341dd9ab8b29f49';
+  String _baseUrl = 'api.themoviedb.org';
+  String _language = 'es-Es';
+
   MoviesProvider() {
     print('Movies provider inicializado');
     this.getOnDisplayMovies();
   }
   getOnDisplayMovies() async {
-    print('getOnDisplayMovies');
+    var url = Uri.https(_baseUrl, '3/movie/now_playing', {
+      '_apiKey': _apiKey,
+      '_language': _language,
+      'page': '1',
+    });
+    final response = await http.get(url);
+    final Map<String, dynamic> decodedData = json.decode(response.body);
+    print(decodedData);
   }
 }
